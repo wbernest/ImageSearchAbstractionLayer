6aca1b037eeaf3482928ca9ef539a119b10b4b4e
Billy Ernest <bernest@gridprotectionalliance.org>
2018-03-02 08:17:31 -0500

    Fixed issue with port maybe not being defined

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(function(err, req, res, next) {
   }  
 })
 
-app.listen(process.env.PORT, function () {
+app.listen((process.env.PORT != undefined? process.env.PORT : '8080'), function () {
   console.log('Node.js listening ...');
 });
 
